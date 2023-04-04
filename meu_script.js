4d5e196efcd4fd5a7b3bb79f3a6e331c0be40465
Ana Almeida <aninhaalmeidav@gmail.com>
2023-04-04 17:51:24 -0300

    dom- manipulando valores de inputs(text)

--- meu_script.js
+++ meu_script.js
@@ -619,3 +619,37 @@ function acao1() {
         //console.log(document.getElementsByTagName('input'))
         //console.log(document.getElementsByClassName('campo_texto'))
         console.log(document.getElementsByName('nome'))
+
+
+        /******manipulando valores de inputs(text)*******/
+        //verifica  o caracter digitado, se é letra vai para o campo 'Letras' se número vai para o campo 'Números'
+
+        function distribuiCaracter() {
+            //selecionar valor digitado
+            var caracter = document.getElementById('entrada').value
+            //limpar o campo de digitação - seleciona e deixa o campo do elemento vazio
+            document.getElementById('entrada').value = ''
+
+            //limpar espaços em branco nas extremidades da string
+            caracter.trim()
+
+            switch (caracter) {
+
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    //adicionar caracterer no campo referente a números
+                    document.getElementById('apenas_numeros').value += caracter
+                    break;
+                default:
+                //adicionar o caracter no campo referente a letra
+                document.getElementById('apenas_letras').value += caracter
+            }
+        }
