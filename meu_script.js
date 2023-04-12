d27a3a06722b7cfb9fe988a3317206aeef78b19b
Ana Almeida <aninhaalmeidav@gmail.com>
2023-04-12 13:52:38 -0300

    laços - prática1 e 2 - encadeados

--- meu_script.js
+++ meu_script.js
@@ -920,3 +920,20 @@ for
 principais, porém não únicos blocos de repetição
  * 
  */
+
+/*lacos_pratica_1*/
+
+var lista_frutas = []
+
+lista_frutas[0] = 'Banana'
+lista_frutas[1] = 'Maçã'
+lista_frutas[2] = 'Morango'
+lista_frutas[3] = 'Uva'
+
+var y = 0
+
+while (y < lista_frutas.length) {
+    //acao
+    document.write(lista_frutas[y] + '<br >')
+    y++
+}
